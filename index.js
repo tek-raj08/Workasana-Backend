eb77263a5690953828711813bd63a63b74880dcd
Tek Raj <ranatekraj06@gmail.com>
2025-06-14 11:20:55 +0530

    CC

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const { userAuth } = require("./middlewares/userAuth")
 
-// const allowedOrigins = ["http://localhost:5173", "https://workasana-frontend-git-main-tek-rajs-projects.vercel.app"]
-// const allowedOrigins = "https://workasana-frontend-git-main-tek-rajs-projects.vercel.app"
-
-
-
-
-// const corsOptions = {
-//    origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true); // Allow the request
-//     } else {
-//       callback(new Error("Not allowed by CORS")); // Block the request
-//     }
-
-//    },
-//    credentials: true,
-//    optionSuccessStatus: 200
-// }
-
 const app = express()
 
 // app.use(
@@ -34,12 +15,14 @@ const app = express()
 //    })
 // )
 
-app.use(
-   cors({
-      origin: "https://workasana-frontend-chi.vercel.app",
-      credentials: true
-   })
-)
+// app.use(
+//    cors({
+//       origin: "https://workasana-frontend-chi.vercel.app",
+//       credentials: true
+//    })
+// )
+
+app.use(cors());
 
 app.use(express.json())
 
